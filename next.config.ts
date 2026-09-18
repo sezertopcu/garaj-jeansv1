bc7867c8bd3f7aa26e33bb73e44d67d7a1371214
Sezer Topcu <pubg252@gmail.com>
2026-09-18 14:35:43 +0300

    Fix iyzipay resources on Vercel

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
 
   serverExternalPackages: ["iyzipay"],
+
+  outputFileTracingIncludes: {
+    "/api/iyzico/initialize": [
+      "./node_modules/iyzipay/lib/**/*",
+    ],
+    "/api/iyzico/callback": [
+      "./node_modules/iyzipay/lib/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
